f858c50ff62e42aff03df00848a959349ebfad0e
salman92-dev <124353689+salman92-dev@users.noreply.github.com>
2026-05-22 15:15:17 +0500

    Add files via upload

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./navbar";
 const Hero = () => {
     return (
-        <div className="flex items-end justify-center h-screen bg-[#241F21] pb-16">
+        <div className="flex items-end justify-center h-[92vh] md:h-screen bg-[#241F21] pb-16">
             <Navbar />
             <div className="">
                 <h1 className="urbanist font-extrabold text-5xl max-md:text-center">
